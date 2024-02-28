d4520cfbc39d20d7b0800cec65202fc7845d227d
Andreas <72671832+ABergli@users.noreply.github.com>
2024-02-28 18:35:15 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
 /*import React from "react";
 import ReactDOM from "react-dom/client";
 import { Application } from "./modules/application/application";
-
 const root = ReactDOM.createRoot(document.getElementById("root")!);
-
 root.render(<Application />);   */
 
 import React from "react";
@@ -11,7 +9,6 @@ import ReactDOM from "react-dom";
 import { Application } from "./modules/application/application";
 
 const root = document.getElementById("root");
-
 ReactDOM.render(
   <React.StrictMode>
     <Application />
