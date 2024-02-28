e462eabc360a02bb42316a8ac09fe3570156efb9
Andreas <72671832+ABergli@users.noreply.github.com>
2024-02-28 15:57:12 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,8 @@
+/*import React from "react";
+import ReactDOM from "react-dom/client";
+import { Application } from "./modules/application/application";
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+root.render(<Application />);   */
 import React from "react";
 import ReactDOM from "react-dom";
 import { Application } from "./modules/application/application";
@@ -8,5 +13,5 @@ ReactDOM.render(
   <React.StrictMode>
     <Application />
   </React.StrictMode>,
-  root
+  root,
 );
